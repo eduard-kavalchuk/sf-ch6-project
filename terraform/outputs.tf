c36e737cef4cea9a42d6b7cc96dca8b46aaeda6e
eduard-kavalchuk <eduardkovalchuk@mail.ru>
2022-10-23 00:07:12 +0300

    Create user on Ubuntu VM, generate ssh keys, install ansible and postgres

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "external_ip_address_vm1_ubuntu" {
+  value = module.vm1.external_ip_address_vm
+}
+
+# output "external_ip_address_vm2_ubuntu" {
+#   value = module.vm2.external_ip_address_vm
+# }
+
+# output "external_ip_address_vm3_centos" {
+#   value = module.vm3.external_ip_address_vm
+# }
